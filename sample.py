d7f2ced1c213f64f3ba55507c8e5e3d6f7ec5b16
Neeraj Gupta <neeraj76@yahoo.com>
2022-11-29 02:39:42 +0530

    Added a comment

--- sample.py
+++ sample.py
@@ -1,3 +1,4 @@
+# Copy the following code into the input box in the index.html file
 import pandas as pd
 df = pd.DataFrame([['one', 'two'], ['three', 'four']], columns=['A', 'B'])
 print(df.to_json(orient='records'))
